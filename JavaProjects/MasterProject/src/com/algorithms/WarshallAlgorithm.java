b67b86b319aa306a817dbb5e0549a88399b7078e
Rohan Verma <rv725555@gmail.com>
2022-08-26 13:02:01 +0530

    improv: Working brute-force implementation of Watshall Algorithm.

--- JavaProjects/MasterProject/src/com/algorithms/WarshallAlgorithm.java
+++ JavaProjects/MasterProject/src/com/algorithms/WarshallAlgorithm.java
@@ -2,11 +2,13 @@ package com.algorithms;
 
 import java.util.Arrays;
 
-//! This algorithm doesn't work properly because if suppose there is a path from 0 to 3, from 3 to 1, from 1 to 2, then the algo isn't able to figure out that there is a path from 0 to 2
+
+//! This is a brute-force implementation since the function is being recursively called until no new paths are being added in the array.
 
 public class WarshallAlgorithm {
+
     public static void main(String[] args) {
-        // Value of 1 at i, j means there lies a path from i to j
+        // Value of 1 at (i, j) means there lies a path from i to j
 
 //        int[][] matrix = {
 //                {1, 1, 0, 1},
@@ -15,31 +17,69 @@ public class WarshallAlgorithm {
 //                {0, 0, 0, 1}
 //        };
 
+
         int[][] matrix = {
                 {0, 0, 0, 1},
                 {0, 0, 1, 1},
                 {0, 1, 1, 0},
                 {1, 1, 0, 1}
         };
+        
+        warshallAlgorithm(matrix, copy2dMatrix(matrix));
 
-        for(int origin = 0; origin < matrix.length; origin++) {
-            for(int destination = 0; destination < (matrix[origin]).length; destination++) {
-                if(matrix[origin][destination] == 1) {
-                    for(int intermediary = 0; intermediary < matrix.length; intermediary++) {
-                        if(matrix[destination][intermediary] == 1) {
-                            matrix[origin][intermediary] = 1;
+        // Output
+        printMatrix(matrix);
+    }
+
+   static void warshallAlgorithm(int[][] resultMatrix, int[][] originalMatrix) {
+        for(int origin = 0; origin < resultMatrix.length; origin++) {
+            for(int destination = 0; destination < (resultMatrix[origin]).length; destination++) {
+                if(resultMatrix[origin][destination] == 1) {
+                    for(int intermediary = 0; intermediary < resultMatrix.length; intermediary++) {
+                        if(resultMatrix[destination][intermediary] == 1) {
+                            resultMatrix[origin][intermediary] = 1;
                         }
                     }
                 }
             }
         }
 
-        // Output
-        printMatrix(matrix);
-    }
+        if(compare2dMatrix(resultMatrix, originalMatrix)) {
+            return;
+        }
+        else {
+            warshallAlgorithm(resultMatrix, copy2dMatrix(resultMatrix)); // passing the result matrix into the original argument for the next call, as a copy to ensure it doesn't get modified.
+        }
+   }
 
-//    static void warshallAlgorithm(int[][] arr)
+   static int[][] copy2dMatrix(int[][] originalArr) {
 
+        int[][] copyArr = new int[originalArr.length][originalArr[0].length];
+        for(int row = 0; row < copyArr.length; row++) {
+            for(int column = 0; column < copyArr[row].length; column++) {
+                copyArr[row][column] = originalArr[row][column];
+            }
+        }
+        return copyArr;
+   }
+
+   // We need to do this because if we use Arrays.equals(), although in the case of integer arrays, the arrays being different objects won't matter. In the case of 2d arrays, the nested array elements will be compared using equals(), returning false since they are two different objects.
+   // TODO : Understand more in-depth
+   static boolean compare2dMatrix(int[][] arr1, int[][] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for(int index = 0; index < arr1.length; index++) {
+            if( !(Arrays.equals(arr1[index], arr2[index])) ) {
+                return false;
+            }
+        }
+
+        return true;
+   }
+
+   // Function for printing the matrix
     static void printMatrix(int[][] arr) {
         for(int row = 0; row < arr.length; row++) {
             for(int column = 0; column < arr[row].length; column++) {
@@ -47,5 +87,7 @@ public class WarshallAlgorithm {
             }
             System.out.println();
         }
+        System.out.println();
+
     }
 }
